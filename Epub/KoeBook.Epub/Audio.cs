f5fea98e55a9a61fa8bc414d18f6ab66059b3247
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-02-21 13:48:44 +0900

    Audioクラス内の処理を変更

--- Epub/KoeBook.Epub/Audio.cs
+++ Epub/KoeBook.Epub/Audio.cs
@@ -3,28 +3,19 @@ using NAudio.Wave;
 
 namespace KoeBook.Epub;
 
-public sealed class Audio(byte[] mp3Data)
+public sealed class Audio
 {
-    private TimeSpan? _totalTime = null;
-    private byte[] _mp3Data = mp3Data;
+    public TimeSpan TotalTime { get; }
+    private byte[] _mp3Data = [];
 
-    public TimeSpan TotalTime
+    public Audio(byte[] mp3Data)
     {
-        get {
-            if (_totalTime.HasValue)
-            {
-               return _totalTime.Value;
-            }
-            else
-            {
-                using var ms = new MemoryStream();
-                ms.Write(_mp3Data.AsSpan());
-                ms.Flush();
-                ms.Position = 0;
-                using var reader = new Mp3FileReader(ms);
-                _totalTime = reader.TotalTime;
-                return _totalTime.Value;
-            }
-        }
+        _mp3Data = mp3Data;
+        using var ms = new MemoryStream();
+        ms.Write(_mp3Data.AsSpan());
+        ms.Flush();
+        ms.Position = 0;
+        using var reader = new Mp3FileReader(ms);
+        TotalTime = reader.TotalTime;
     }
 }
