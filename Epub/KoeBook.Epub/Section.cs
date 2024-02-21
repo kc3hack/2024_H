10353dd4738183ab04a8b4f600170a980c5cfd69
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-02-21 15:48:27 +0900

    sectionの書き出し

--- Epub/KoeBook.Epub/Section.cs
+++ Epub/KoeBook.Epub/Section.cs
@@ -1,4 +1,6 @@
-﻿namespace KoeBook.Epub;
+﻿using System.Text;
+
+namespace KoeBook.Epub;
 
 public sealed class Section
 {
@@ -10,5 +12,72 @@ public sealed class Section
     {
         Title = title;
         Id = Guid.NewGuid().ToString();
+    }
+
+    public string CreateSectionXhtml(string? cssPath)
+    {
+        var builder = new StringBuilder($"""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+                <head>  
+                    {(cssPath != null ? $"<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{cssPath}\"/>" : "")}
+                    <title>{Title}</title> 
+                </head> 
+                <body>
+            """);
+
+        for (int i = 0; i < Elements.Count; i++)
+        {
+            if (Elements[i] is Paragraph para)
+            {
+                builder.AppendLine($"""
+                            <p id="{Id}_p{i}" {(para.ClassName != null ? $"class=\"{para.ClassName}\"" : "")}>
+                                {para.Text}
+                            </p>
+                    """);
+            }
+            else if (Elements[i] is Picture pic)
+            {
+                builder.AppendLine($"""
+                            <p id="{Id}_p{i}" {(pic.ClassName != null ? $"class=\"{pic.ClassName}\"" : "")}>
+                                <img src="{Path.GetFileName(pic.PictureFilePath)}"
+                            </p>
+                    """);
+            }
+        }
+
+        builder.AppendLine("""
+                </body>
+            </html>
+            """);
+        return builder.ToString();
+    }
+
+    public string CreateSectionSmil(Section section)
+    {
+        var builder = new StringBuilder($"""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <smil xmlns="http://www.w3.org/ns/SMIL" version="3.0">
+                <body>
+            """);
+
+        for (int i = 0; i < section.Elements.Count; i++)
+        {
+            if (section.Elements[i] is Paragraph para)
+            {
+                builder.AppendLine($"""
+                    <par id="{Id}_p{i}_audio" {(para.ClassName != null ? $"class=\"{para.ClassName}\"" : "")}>
+                        <text src="{Id}.xhtml#{Id}_p{i}" />
+                        <audio clipBegin="0s" clipEnd="{para.Audio?.TotalTime}" src="{Id}_p{i}.mp3"/>
+                    </par>
+            """);
+            }
+        }
+
+        builder.AppendLine("""
+                </body>
+            </smil>
+            """);
+        return builder.ToString();
     }
 }
