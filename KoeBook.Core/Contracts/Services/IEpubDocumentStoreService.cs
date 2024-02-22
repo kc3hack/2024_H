6eafa6979b6587b8304745c78c78a38e99f39da9
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-02-22 16:47:57 +0900

    EpubDocumentStoreServiceの作成

---
+++ KoeBook.Core/Contracts/Services/IEpubDocumentStoreService.cs
@@ -0,0 +1,12 @@
+﻿using KoeBook.Epub;
+
+namespace KoeBook.Core.Contracts.Services;
+
+public interface IEpubDocumentStoreService
+{
+    IReadOnlyList<EpubDocument> Documents { get; }
+
+    void Register(EpubDocument document);
+
+    void Unregister(Guid id);
+}
