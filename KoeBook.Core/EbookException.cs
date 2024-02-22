41de641ca5fd5edc83f5f73fd4e44f5e383f266d
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-02-22 21:52:13 +0900

    EpubGenerateServiceの実装

---
+++ KoeBook.Core/EbookException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace KoeBook.Core;
+
+public class EbookException : Exception
+{
+    public override string? Message { get; }
+
+    public ExceptionType ExceptionType { get; }
+
+    public EbookException(ExceptionType exceptionType, string? message = null, Exception? innerException = null) : base(null, innerException)
+    {
+        Message = message;
+        ExceptionType = exceptionType;
+    }
+}
+
+public enum ExceptionType
+{
+    [EnumMember(Value = "Epubの生成に失敗しました")]
+    EpubCreateError
+}
---
+++ KoeBook.Core/Services/EpubGenerateService.cs
@@ -0,0 +1,45 @@
+﻿using KoeBook.Core.Contracts.Services;
+using KoeBook.Core.Models;
+using KoeBook.Epub;
+
+namespace KoeBook.Core.Services;
+
+public class EpubGenerateService(ISoundGenerationService soundGenerationService, IEpubDocumentStoreService epubDocumentStoreService) : IEpubGenerateService
+{
+    private ISoundGenerationService _soundGenerationService = soundGenerationService;
+    private IEpubDocumentStoreService _documentStoreService = epubDocumentStoreService;
+
+    public async ValueTask<string> GenerateEpubAsync(BookScripts bookScripts, string tempDirectory, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var document = _documentStoreService.Documents.Where(doc=>doc.Id == bookScripts.BookProperties.Id).FirstOrDefault()
+            ?? throw new InvalidOperationException($"The epub document ({bookScripts.BookProperties.Id}) can't be found.");
+
+        for (int i = 0; i < document.Chapters.Count; i++)
+        {
+            for (int j = 0; j < document.Chapters[i].Sections.Count; j++)
+            {
+                for (int k = 0; k< document.Chapters[i].Sections[j].Elements.Count; k++)
+                {
+                    if (document.Chapters[i].Sections[j].Elements[k] is Paragraph para)
+                    {
+                        var scriptLine = bookScripts.ScriptLines.Where(sl => sl.Id == $"{document.Chapters[i].Sections[j].Id}_p{k}").FirstOrDefault();
+                        if (scriptLine != null) {
+                            para.Audio = new Audio(await _soundGenerationService.GenerateLineSoundAsync(scriptLine, bookScripts.Options, cancellationToken));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (await document.TryCreateEpubAsync(tempDirectory, bookScripts.BookProperties.Id.ToString(), cancellationToken))
+        {
+            return Path.Combine(tempDirectory, $"{bookScripts.BookProperties.Id}.epub");
+        }
+        else
+        {
+            throw new EbookException(ExceptionType.EpubCreateError);
+        }
+    }
+}
