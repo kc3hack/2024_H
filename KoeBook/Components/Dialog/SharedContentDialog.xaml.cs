137479cffe9ca50af0297381743a06a824d79a16
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2024-02-23 10:30:16 +0900

    #5-2 ダイアログのテーマを修正

---
+++ KoeBook/Components/Dialog/SharedContentDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace KoeBook.Components.Dialog;
+
+public sealed partial class SharedContentDialog : ContentDialog
+{
+    public SharedContentDialog(string title, string primaryText, string closeText, ContentDialogButton defaultButton)
+    {
+        Title = title;
+        PrimaryButtonText = primaryText;
+        CloseButtonText = closeText;
+        DefaultButton = defaultButton;
+        InitializeComponent();
+    }
+}
