09bca01d6dcc53884b82d1cf2ab1706bfd7107a2
TakenPt <takekimu14@gmail.com>
2024-02-24 16:50:50 +0900

    wip

---
+++ Epub/KoeBook.Epub/ScrapingHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Reflection.Metadata;
+
+namespace KoeBook.Epub;
+
+internal static class ScrapingHelper
+{
+    internal static void checkChapter(EpubDocument document)
+    {
+        if (document.Chapters.Count == 0)
+        {
+            document.Chapters.Add(new Chapter() { Title = null });
+        }
+
+        return;
+
+    }
+
+    internal static void checkSection(EpubDocument document, int ChapterNum)
+    {
+
+        checkChapter(document);
+
+        if (document.Chapters[ChapterNum].Sections.Count == 0)
+        {
+            if (document.Chapters[ChapterNum].Title != null)
+            {
+                document.Chapters[ChapterNum].Sections.Add(new Section(document.Chapters[ChapterNum].Title!));
+            } else
+            {
+                document.Chapters[ChapterNum].Sections.Add(new Section(document.Title));
+            }
+            
+        }
+        return;
+    }
+
+    internal static void checkParagraph(EpubDocument document, int chapterNum, int sectionNum)
+    {
+        checkSection(document, chapterNum);
+        if (document.Chapters[chapterNum].Sections[sectionNum].Elements.Count == 0)
+        {
+            document.Chapters[chapterNum].Sections[sectionNum].Elements.Add(new Paragraph());
+        }
+        return;
+    }
+}
