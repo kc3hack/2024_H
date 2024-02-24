c4bf4e75a8b803af949ad231328c824f6863e3d3
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-02-24 15:02:30 +0900

    仮

---
+++ KoeBook.Core/Services/AnalyzerService.cs
@@ -0,0 +1,19 @@
+﻿using KoeBook.Core.Contracts.Services;
+using KoeBook.Core.Models;
+using KoeBook.Epub.Service;
+
+namespace KoeBook.Core.Services;
+
+public class AnalyzerService(IScrapingService scrapingService, IEpubDocumentStoreService epubDocumentStoreService) : IAnalyzerService
+{
+    private readonly IScrapingService _scrapingService = scrapingService;
+    private readonly IEpubDocumentStoreService _epubDocumentStoreService = epubDocumentStoreService;
+
+    public async ValueTask<BookScripts> AnalyzeAsync(BookProperties bookProperties, string tempDirectory, string coverFilePath, CancellationToken cancellationToken)
+    {
+        var document = await _scrapingService.ScrapingAsync(bookProperties.Source, coverFilePath, bookProperties.Id, cancellationToken);
+        _epubDocumentStoreService.Register(document, cancellationToken);
+
+        throw new NotImplementedException();
+    }
+}
